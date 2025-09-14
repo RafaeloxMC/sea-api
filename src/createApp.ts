0f3241640de41eff94a012aa91595be4014595b7
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-14 16:56:14 +0200

    Fix issue with CORS on browser export

--- src/createApp.ts
+++ src/createApp.ts
@@ -3,3 +3,10 @@ import express from "express";
 export const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	next();
+});
